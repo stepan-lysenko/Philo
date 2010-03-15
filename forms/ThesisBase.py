4e357d2107e4ac7bae9f144259b855ed583116d1
Stepan Lysenko <stepan.lysenko@gmail.com>
2010-03-15 11:03:16 +0600

    ThesisBase added

---
+++ forms/ThesisBase.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+
+class ThesisBase:
+    ListNames = []
+    def __init__(self):
+        return
+    def getListNames(self)
+        return
+    def getDesc(self, name)
+        return
+    def setDesc(self, name, newDesc)
+        return
+    def rename(self, name, newName)
+        return
+    def addThesis(self, name, desc)
+        return
+    def delThesis(self, name)
+        return
