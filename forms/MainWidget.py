72654d78179b4146c2089ae313bd062a8693cdc4
Stepan Lysenko <stepan.lysenko@gmail.com>
2010-03-16 09:19:41 +0600

    Now new thesis names are increasing

--- forms/MainWidget.py
+++ forms/MainWidget.py
@@ -54,13 +54,28 @@ class PhiloTab(QtGui.QWidget):
         Box.addWidget(self.teThesisView)
         Box.addItem(spacer2)
 
+    def SearchName(self, name):
+        for i in xrange(self.lvThesis.count()):
+            if name == self.lvThesis.item(i).text():
+                return 1
+        return 0
+                
+
     def SelectionChanged(self):
         self.currentItem.desc = self.teThesisView.toPlainText()
         self.currentItem = self.lvThesis.currentItem()
         self.teThesisView.setText(self.currentItem.desc)
 
     def AddNewThesis(self):
-        tmp = QtGui.QListWidgetItem("New Thesis")
+        i = 0
+        name = 'New Thesis'
+        tmp = ''
+        while self.SearchName(name + tmp) == 1:
+            i += 1
+            tmp = str(i)
+        if i != 0:
+            name += str(i)
+        tmp = QtGui.QListWidgetItem(name)
         tmp.setFlags(QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsSelectable
                                                     | QtCore.Qt.ItemIsEnabled)
         tmp.desc = ''
