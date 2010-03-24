3037abe449ec6aa23e297b308a137bbd3bd7b41e
Stepan Lysenko <stepan.lysenko@gmail.com>
2010-03-24 15:42:36 +0600

    Items can be a moved ugly.

--- forms/SchemeView.py
+++ forms/SchemeView.py
@@ -6,25 +6,55 @@ from PyQt4 import QtCore, QtGui
 class SchemeView(QtGui.QGraphicsView):
     def __init__(self):
         QtGui.QGraphicsView.__init__(self)
+        self.setBackgroundBrush(QtGui.QBrush())
+
         self.scene = QtGui.QGraphicsScene()
         self.setScene(self.scene)
+        self.scene.setSceneRect(QtCore.QRectF(-50, -50, 100, 100))
+
         self.setRenderHint(QtGui.QPainter.Antialiasing)
-        item1 = ThesisView(QtCore.Qt.green)
+
+        item1 = ThesisView(u'Гиперэкстензия')
         item1.setPos(0, 0)
         self.scene.addItem(item1)
-        item2 = ThesisView(QtCore.Qt.blue)
+
+        item2 = ThesisView(u'Эллада')
         item2.setPos(-40, 40)
         self.scene.addItem(item2)
 
+    def dropEvent(self, event):
+        print 'dropEvent in Scheme'
+
 class ThesisView(QtGui.QGraphicsItem):
-    def __init__(self, color=QtCore.Qt.white):
+    def __init__(self, text = '', color = QtCore.Qt.white):
         QtGui.QGraphicsItem.__init__(self)
+        self.setCursor(QtCore.Qt.OpenHandCursor)
         self.color = color
+        self.text = text
+
+    def setColor(self, color):
+        self.color = color
+
+    def setText(self, text):
+        self.text = text
 
     def paint(self, painter, option, widget):
         painter.setPen(QtGui.QPen(QtCore.Qt.black, 1))
         painter.setBrush(QtGui.QBrush(self.color))
-        painter.drawRect(0, 0, 40, 40)
+        painter.drawRect(-20, -20, 40, 40)
+        painter.drawText(QtCore.QRectF(-20, -20, 40, 40),
+                            QtCore.Qt.AlignCenter, 'text')
+
+    def mousePressEvent(self, event):
+        if event.button() != QtCore.Qt.LeftButton:
+            event.ignore()
+            return
+        self.setCursor(QtCore.Qt.ClosedHandCursor)
+
+    def mouseReleaseEvent(self, event):
+        self.setCursor(QtCore.Qt.OpenHandCursor)
+
+    
 
     def boundingRect(self):
-        return QtCore.QRectF(-15.5, -15.5, 34, 34);
+        return QtCore.QRectF(-20.5, -20.5, 41, 41);
