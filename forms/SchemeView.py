d81b9a275de47f415a1f0e1c6eca172e1377d64a
Stepan Lysenko <stepan.lysenko@gmail.com>
2010-03-24 11:37:42 +0600

    ThesisView added.

--- forms/SchemeView.py
+++ forms/SchemeView.py
@@ -9,4 +9,22 @@ class SchemeView(QtGui.QGraphicsView):
         self.scene = QtGui.QGraphicsScene()
         self.setScene(self.scene)
         self.setRenderHint(QtGui.QPainter.Antialiasing)
-        self.scene.addText(u'Хуй')
+        item1 = ThesisView(QtCore.Qt.green)
+        item1.setPos(0, 0)
+        self.scene.addItem(item1)
+        item2 = ThesisView(QtCore.Qt.blue)
+        item2.setPos(-40, 40)
+        self.scene.addItem(item2)
+
+class ThesisView(QtGui.QGraphicsItem):
+    def __init__(self, color=QtCore.Qt.white):
+        QtGui.QGraphicsItem.__init__(self)
+        self.color = color
+
+    def paint(self, painter, option, widget):
+        painter.setPen(QtGui.QPen(QtCore.Qt.black, 1))
+        painter.setBrush(QtGui.QBrush(self.color))
+        painter.drawRect(0, 0, 40, 40)
+
+    def boundingRect(self):
+        return QtCore.QRectF(-15.5, -15.5, 34, 34);
